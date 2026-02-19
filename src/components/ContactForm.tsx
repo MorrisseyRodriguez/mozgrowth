0a6e6f47d76329bfbd83e820a1e373769b47deb1
MorrisseyRodriguez <mozr15294@gmail.com>
2026-02-18 20:49:21 -0800

    Update recipient email to morrisseyr@moz-growth.com

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -33,6 +33,7 @@ const ContactForm = () => {
           from_name: formData.name,
           company_name: formData.company,
           from_email: formData.email,
+          to_email: 'morrisseyr@moz-growth.com',
           message: `Discovery Call Request from ${formData.name} at ${formData.company}`
         },
         'OZo1S52ylqKZv5AWM'
